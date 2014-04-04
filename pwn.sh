3fbaad67745398fc7059be66f18ebfd16469efc9
dogera1n <raindoge@gmail.com>
2014-04-03 23:07:57 -0500

    Run this in terminal as own.sh

---
+++ pwn.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# d0gecl0udbyp4ss copyright 2014 d0ge
+echo "powered by d0ge"
+echo "Welcome to d0gecl0udbyp4ss. The utility will automatically start in the next 5 seconds."
+echo "Connecting to your device...Please wait."
+sleep 10
+echo "Device connected! Sending payload data..."
+echo "Writing offsets to 0x000001"
+sleep 1
+echo "Writing offsets to 0x000010"
+sleep 0.5
+echo "Writing offsets to 0x000100"
+sleep 0.5
+echo "Writing offsets to 0x001000"
+sleep 0.5
+echo "Writing offsets to 0x010000"
+sleep 0.5
+echo "Writing offsets to 0x100000"
+sleep 0.5
+echo "Writing offsets to 1x000000"
+sleep 0.5
+echo "Writing offsets to 1x100000"
+sleep 0.5
+echo "Writing offsets to 1x110000"
+sleep 0.5
+echo "Writing offsets to 1x111000"
+sleep 0.5
+echo "Writing offsets to 1x111100"
+sleep 0.5
+echo "Writing offsets to 1x111110"
+sleep 0.5
+echo "Writing offsets to 1x111111"
+sleep 0.5
+echo "iBoot has returned a kernel fault address. Please let d0ge check if this is the right address."
+sleep 1
+echo "reading..."
+sleep 1
+echo "Fatal segment overflow has occurred (this is good)"
+sleep 1
+echo "Reading segment...."
+sleep 1
+echo "SEGMENT OVERFLOW at 1x11111"
+sleep 1
+echo "Ready to run d0ge-injection!!"
+sleep 5
+echo "Running d0ge-injection..."
+sleep 2
+echo "waiting on device.."
+sleep 10
+echo "Running second stage packet filter..."
+sleep 2
+echo "Contacting Apple Activation server to pull tickets."
+sleep 0.5
+echo "Please wait..."
+sleep 1
+echo "Preparing for response..."
+sleep 0.5
+echo "Server is responding...yay"
+sleep 0.5
+echo "Receiving tickets"
+sleep 5
+echo "Ticket fetch was a success! Ready to activate...."
+sleep 2
+echo "Running d0gecl0udbyp4ss..."
+sleep 2
+echo "Please wait as d0gecl0udbyp4ss finishes up"
+sleep 5
+echo "Please wait as your device bypasses the Activation Lock screen. This may take up to 2 mintues."
+sleep 120
+echo "Waiting on Device..."
+sleep 2
+echo "Activation Bypass was a success! Your device will reboot soon. This may take up to 30 mintues. You can now exit this utility."
+sleep 30
+echo "APRIL FOOLS KIDS!!! xD!!!"
