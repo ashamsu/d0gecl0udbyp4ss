e73caf34eb5fdf5afe01b4d9b909f3ebd657c5b5
dogera1n <raindoge@gmail.com>
2014-04-04 21:55:51 -0500

    Base file structure for pwn.bat

---
+++ windows_base.bat
@@ -0,0 +1,72 @@
+echo powered by d0ge >&2
+echo Welcome to d0gecl0udbyp4ss. The utility will automatically start in the next 5 seconds. >&2
+echo Connecting to your device...Please wait.>&2
+timeout /t 10 /nobreak > NUL
+echo Device connected! Sending payload data... >&2
+echo Writing offsets to 0x000001 >&2
+timeout /t 1 /nobreak > NUL
+echo Writing offsets to 0x000010 >&2
+timeout /t 1 /nobreak > NUL
+echo Writing offsets to 0x000100 >&2
+timeout /t 1 /nobreak > NUL
+echo Writing offsets to 0x001000 >&2
+timeout /t 1 /nobreak > NUL
+echo Writing offsets to 0x010000 >&2
+timeout /t 1 /nobreak > NUL
+echo Writing offsets to 0x100000 >&2
+timeout /t 1 /nobreak > NUL
+echo Writing offsets to 1x000000 >&2
+timeout /t 1 /nobreak > NUL
+echo Writing offsets to 1x100000 >&2
+timeout /t 1 /nobreak > NUL
+echo Writing offsets to 1x110000 >&2
+timeout /t 1 /nobreak > NUL
+echo Writing offsets to 1x111000 >&2
+timeout /t 1 /nobreak > NUL
+echo Writing offsets to 1x111100 >&2
+timeout /t 1 /nobreak > NUL
+echo Writing offsets to 1x111110 >&2
+timeout /t 1 /nobreak > NUL
+echo Writing offsets to 1x111111 >&2
+timeout /t 1 /nobreak > NUL
+echo iBoot has returned a kernel fault address. Please let d0ge check if this is the right address. >&2
+timeout /t 1 /nobreak > NUL
+echo reading... >&2
+timeout /t 1 /nobreak > NUL
+echo Fatal segment overflow has occurred (this is good) >&2
+timeout /t 1 /nobreak > NUL
+echo Reading segment....>&2
+timeout /t 1 /nobreak > NUL
+echo SEGMENT OVERFLOW at 1x11111 >&2
+timeout /t 1 /nobreak > NUL
+echo Ready to run d0ge-injection!! >&2
+timeout /t 5 /nobreak > NUL
+echo Running d0ge-injection... >&2
+timeout /t 2 /nobreak > NUL
+echo waiting on device.. >&2
+timeout /t 10 /nobreak > NUL
+echo Running second stage packet filter... >&2
+timeout /t 2 /nobreak > NUL
+echo Contacting Apple Activation server to pull tickets. >&2
+timeout /t 1 /nobreak > NUL
+echo Please wait... >&2
+timeout /t 1 /nobreak > NUL
+echo Preparing for response... >&2
+timeout /t 1 /nobreak > NUL
+echo Server is responding...yay >&2
+timeout /t 1 /nobreak > NUL
+echo Receiving tickets >&2
+timeout /t 5 /nobreak > NUL
+echo Ticket fetch was a success! Ready to activate.... >&2
+timeout /t 2 /nobreak > NUL
+echo Running d0gecl0udbyp4ss... >&2
+timeout /t 2 /nobreak > NUL
+echo Please wait as d0gecl0udbyp4ss finishes up >&2
+timeout /t 5 /nobreak > NUL
+echo Please wait as your device bypasses the Activation Lock screen. This may take up to 2 mintues. >&2
+timeout /t 120 /nobreak > NUL
+echo Waiting on Device...
+timeout /t 3 /nobreak > NUL
+echo Activation Bypass was a success! Your device will reboot soon. This may take up to 30 mintues. You can now exit this utility. >&2
+timeout /t 1 /nobreak > NUL
+echo APRIL FOOLS KIDS!!! xD!!! >&2
